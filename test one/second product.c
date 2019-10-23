b5af25a781eb39d3dca331dc00f105112f4e56f0
Andrew pegg <andrewcomputsci2019@gmail.com>
2019-10-23 14:31:52 -0700

    oooh

---
+++ test one/second product.c
@@ -0,0 +1,9 @@
+//
+//  second product.c
+//  test one
+//
+//  Created by Pegg, Andrew on 10/23/19.
+//  Copyright © 2019 Pegg, Andrew. All rights reserved.
+//
+
+#include "second product.h"
---
+++ test one/second product.h
@@ -0,0 +1,14 @@
+//
+//  second product.h
+//  test one
+//
+//  Created by Pegg, Andrew on 10/23/19.
+//  Copyright © 2019 Pegg, Andrew. All rights reserved.
+//
+
+#ifndef second_product_h
+#define second_product_h
+
+#include <stdio.h>
+
+#endif /* second_product_h */
