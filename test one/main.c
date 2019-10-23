4a5b7ccbcf9d38c65ab3c6399ba9b64a3b5fc62d
Andrew pegg <andrewcomputsci2019@gmail.com>
2019-10-23 14:28:11 -0700

    oooooooff

--- test one/main.c
+++ test one/main.c
@@ -8,8 +8,9 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main()
+{
+    printf("Original project");
+    project2();
+    
 }
